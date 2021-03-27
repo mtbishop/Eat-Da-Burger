3ebb22995b0148a4c0dd17b1737e2256608082ab
mtbishop <bishopmatthew76@gmail.com>
2021-03-27 13:16:31 -0500

    jawsdb push

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,13 +1,15 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
   user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'LucidDream729K',
-  database: 'burgers_db',
+  password: 'rootroot',
+  database: 'burgers_db'
 });
 
 // Make connection.
